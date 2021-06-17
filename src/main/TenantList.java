c0fd3025e71e098c243f3a19d1812b66a3e6fc2e
RosianPolar <iancoco19@gmail.com>
2021-06-17 12:56:11 -0700

    Coded ability to read listOfTenants in TenantList

--- src/main/TenantList.java
+++ src/main/TenantList.java
@@ -1,11 +1,29 @@
 package main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TenantList {
     private ArrayList<Tenant> tenantList;
 
     public void display(){
+        //Retrieving all Tenants Stored.
+        try {
+            File myObj = new File("listOfTenants.txt");
+            Scanner scan = new Scanner(myObj);
+            while (scan.hasNextLine()) {
+                String data = scan.nextLine();
+                String[] tenant = data.split(",");
+                tenantList.add(new Tenant(tenant[0], Integer.parseInt(tenant[1])));
+            }
+            scan.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+            e.printStackTrace();
+        }
+
         System.out.println("Displaying the list of all Tenants:\n");
         for(Tenant t : tenantList){
             System.out.print("AptNo: "+ t.getAptNumber() + " : " + t.getName());
