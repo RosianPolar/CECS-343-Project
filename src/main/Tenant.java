29e61fa9525a29cbace4fd1c897d7d9219afbc6b
RosianPolar <iancoco19@gmail.com>
2021-06-17 12:36:32 -0700

    Finished Tenant.java

--- src/main/Tenant.java
+++ src/main/Tenant.java
@@ -1,5 +1,23 @@
 package main;
 
 public class Tenant{
+    private String name;
+    private int aptNumber;
 
+    public Tenant(){
+        name = "";
+        aptNumber = 0;
+    };
+
+    public Tenant(String n, int apn ){
+        name = n;
+        aptNumber = apn;
+    }
+
+    public int getAptNumber(){
+        return aptNumber;
+    }
+    public String getName(){
+        return name;
+    }
 }
