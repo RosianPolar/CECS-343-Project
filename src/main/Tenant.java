93fae9f13ceb8cd925366275c9a7d7ee149548df
RosianPolar <iancoco19@gmail.com>
2021-06-23 12:51:06 -0700

    Implemented getters and setters for both Tenant and Expense

--- src/main/Tenant.java
+++ src/main/Tenant.java
@@ -7,17 +7,26 @@ public class Tenant{
     public Tenant(){
         name = "";
         aptNumber = 0;
-    };
+    }
 
     public Tenant(String n, int apn ){
         name = n;
         aptNumber = apn;
     }
 
-    public int getAptNumber(){
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAptNumber() {
         return aptNumber;
     }
-    public String getName(){
-        return name;
+
+    public void setAptNumber(int aptNumber) {
+        this.aptNumber = aptNumber;
     }
 }
