a666637a9801dba1c6eeabfaa1bc3e8ff7f8d791
RosianPolar <iancoco19@gmail.com>
2021-06-24 09:54:52 -0700

    Finished ability to display the AnnualReport and used some text blocks in the UserInterface

--- src/main/AnnualReport.java
+++ src/main/AnnualReport.java
@@ -1,5 +1,32 @@
 package main;
 
-public class AnnualReport{
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class AnnualReport{
+    public void display(){
+        //todo Calculate all fields in the annual report
+
+
+
+        try{
+        File myObj = new File("main/annualReport");
+        Scanner scan = new Scanner(myObj);
+        System.out.println(scan.nextLine());
+        System.out.println(scan.nextLine());
+
+        while (scan.hasNextLine()) {
+            String data = scan.nextLine();
+            String[] expense = data.split(",");
+            System.out.println(expense[0] + ":" + expense[1]);
+        }
+        System.out.println();
+        scan.close();
+    } catch (
+    FileNotFoundException e) {
+        System.out.println("File not found.");
+        e.printStackTrace();
+    }
+    }
 }
--- src/main/UserInterface.java
+++ src/main/UserInterface.java
@@ -8,15 +8,17 @@ public class UserInterface{
     }
 
     public void interact(){
-        System.out.print("Welcome to the Rental management program.\n" +
-                "Please select an option to continue:\n" +
-                "1) Display the Tenant List\n" +
-                "2) Input a rental payment\n" +
-                "3) Display the Expense Record\n" +
-                "4) Display the Annual Summary\n" +
-                "5) Display the Rent Record\n" +
-                "6) Add a new Tenant\n" +
-                "7) Quit\n");
+        System.out.print("""
+                Welcome to the Rental management program.
+                Please select an option to continue:
+                1) Display the Tenant List
+                2) Input a rental payment
+                3) Display the Expense Record
+                4) Display the Annual Summary
+                5) Display the Rent Record
+                6) Add a new Tenant
+                7) Quit
+                """);
 
         Scanner in = new Scanner(System.in);
         int input = in.nextInt();
@@ -37,6 +39,9 @@ public class UserInterface{
                     er.display();
                     break;
                 case 4:
+                    System.out.println("Displaying the Annual Report:");
+                    AnnualReport ar = new AnnualReport();
+                    ar.display();
                     break;
                 case 5:
                     System.out.println("Displaying the Rent Record:");
@@ -54,14 +59,15 @@ public class UserInterface{
                     break;
             }
             System.out.println(
-                    "Please select an option to continue:\n" +
-                    "1) Display the Tenant List\n" +
-                    "2) Input a rental payment\n" +
-                    "3) Display the Expense Record\n" +
-                    "4) Display the Annual Summary\n" +
-                    "5) Display the Rent Record\n" +
-                    "6) Add a new Tenant\n" +
-                    "7) Quit");
+                    """
+                            Please select an option to continue:
+                            1) Display the Tenant List
+                            2) Input a rental payment
+                            3) Display the Expense Record
+                            4) Display the Annual Summary
+                            5) Display the Rent Record
+                            6) Add a new Tenant
+                            7) Quit""");
             input = in.nextInt();
         }
 
