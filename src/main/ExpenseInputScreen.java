94bc776d572574ce55496dbf3b30b0cde474f682
Setha9 <80129057+Setha9@users.noreply.github.com>
2021-06-25 21:12:54 +0700

    Finished with the expenseInputScreen

--- src/main/ExpenseInputScreen.java
+++ src/main/ExpenseInputScreen.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.FilenotFoundException;
 import java.io.IOException;
 
-public class ExpenseInputScreen extends expense{
+public class ExpenseInputScreen extends Expense{
   private double newExpenseInfo;
   
   public void display(){
@@ -13,9 +13,27 @@ public class ExpenseInputScreen extends expense{
         Scanner scan = new Scanner (System.in);
         char control = 'n';
         do {
-          System.out.println ("Please Enter the ")
-        }
+          System.out.println ("Please enter the expense amount in number: ");
+            while (scan.hasNextDouble())
+            {
+             newExpenseInfo = scan.nextDouble ();
+            }
+              ex.write(newExpenseInfo);
+            System.out.println ("Would you like to add more expenses? " + 
+                                "\n 'Y/y' or for Yes and 'N/n' to leave the Expense Input Screen")?
+              while (scan.hasNextLine ())
+              {
+                control = scan.nextLine().charAt(0);
+              }
+           } while (control!= 'n' || control!= 'N';)
+        } catch (FileNotFoundException e){
+          e.printStackTrace ();    
+        } catch (IOException e) {
+          e.printStackTrace ();
+    }
   
   }
+  
+}
   
 }
