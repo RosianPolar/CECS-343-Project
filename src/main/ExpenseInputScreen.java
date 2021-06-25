602684060aaefbbc3428e56b48b4fa47c98d0700
Setha9 <80129057+Setha9@users.noreply.github.com>
2021-06-25 20:44:14 +0700

    To continue

--- src/main/ExpenseInputScreen.java
+++ src/main/ExpenseInputScreen.java
@@ -5,9 +5,15 @@ import java.io.FilenotFoundException;
 import java.io.IOException;
 
 public class ExpenseInputScreen extends expense{
+  private double newExpenseInfo;
+  
   public void display(){
     try {
-        File     
+        FileWriter ex = new FileWriter ("expense.txt");
+        Scanner scan = new Scanner (System.in);
+        char control = 'n';
+        do {
+          System.out.println ("Please Enter the ")
         }
   
   }
