5f5aa7958c1bdf59d4b8d5d05faa225c3ee32299
mOrteg4 <73629210+mOrteg4@users.noreply.github.com>
2021-06-29 13:12:32 -0700

    changed float to int for apt no and fw.close()

--- src/main/TenantInputScreen.java
+++ src/main/TenantInputScreen.java
@@ -5,37 +5,34 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 public class TenantInputScreen extends Tenant{
-        private String newTenantFName;
-        private float newTenantAptNo;
 
 
         public void addTenant() throws IOException {
                 try {
                         FileWriter fw = new FileWriter("listOfTenants.txt");
                         Scanner scan = new Scanner(System.in);
-                        String control = "";
+                        String control;
                         do {
                                 System.out.println("Please enter the Tenant's Full Name \n");
-                                while(scan.hasNextLine())
-                                {
-                                        newTenantFName = scan.nextLine();
-                                }
+
+                                String newTenantFName = scan.nextLine();
+
                                 System.out.println("Please enter the Tenant's Apartment Number: ");
-                                while(scan.hasNextFloat())
-                                {
-                                        newTenantAptNo = scan.nextFloat();
-                                }
-                                        String currTenantAptNo = String.valueOf(newTenantAptNo);
+
+                                int newTenantAptNo = scan.nextInt();
+
+                                        String currTenantAptNo = Integer.toString(newTenantAptNo);
                                         fw.write(newTenantFName);
                                         fw.write(currTenantAptNo);
                                 System.out.println("Would you like to add another Tenant? " +
                                         "\n 'Y/y' or ANY key BUT 'N/n' to add | 'N/n' to quit the Tenant Input Screen");
-                                while(scan.hasNextLine())
-                                {
+
+                                        scan.nextLine();
                                         control = scan.nextLine();
                                         control = control.toLowerCase().substring(0,1);
-                                }
+
                         } while (!control.equals("n"));
+                        fw.close();
                 } catch (FileNotFoundException e) {
                         e.printStackTrace();
                 }
