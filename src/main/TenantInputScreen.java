e78b00f29c6e4640cb7f1d94742e292e80d579ef
Damar Rodas <31608436+damar100@users.noreply.github.com>
2021-06-29 15:52:12 -0700

    update on print method
    changed printers to save previous inputs in txt

--- src/main/TenantInputScreen.java
+++ src/main/TenantInputScreen.java
@@ -3,36 +3,44 @@ package main;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 public class TenantInputScreen extends Tenant{
+        private String newTenantFName;
+        private int newTenantAptNo;
 
 
         public void addTenant() throws IOException {
                 try {
-                        FileWriter fw = new FileWriter("listOfTenants.txt");
+                       
+                        PrintWriter rf = new PrintWriter (new FileWriter("main/listOfTenants", true));
                         Scanner scan = new Scanner(System.in);
-                        String control;
+                        String control = "";
                         do {
                                 System.out.println("Please enter the Tenant's Full Name \n");
-
-                                String newTenantFName = scan.nextLine();
-
-                                System.out.println("Please enter the Tenant's Apartment Number: ");
-
-                                int newTenantAptNo = scan.nextInt();
-
+                                
+                                
+                                        newTenantFName = scan.nextLine();
+                                
+                                        System.out.println("Please enter the Tenant's Apartment Number: ");
+                                
+                                
+                                        newTenantAptNo = scan.nextInt();
+                                
                                         String currTenantAptNo = Integer.toString(newTenantAptNo);
-                                        fw.write(newTenantFName);
-                                        fw.write(currTenantAptNo);
-                                System.out.println("Would you like to add another Tenant? " +
+                                        rf.write(currTenantAptNo);
+                                        rf.write("\t");
+                                        rf.write(newTenantFName);
+                                        rf.write("\n");
+                                        
+                                        System.out.println("Would you like to add another Tenant? " +
                                         "\n 'Y/y' or ANY key BUT 'N/n' to add | 'N/n' to quit the Tenant Input Screen");
-
+                                
                                         scan.nextLine();
                                         control = scan.nextLine();
                                         control = control.toLowerCase().substring(0,1);
-
-                        } while (!control.equals("n"));
-                        fw.close();
+                                
+                        } while (!control.equals("n")); rf.close();
                 } catch (FileNotFoundException e) {
                         e.printStackTrace();
                 }
