0e1f15c40bb70455fd490325261f3c6477dbc2f6
Damar Rodas <31608436+damar100@users.noreply.github.com>
2021-06-28 18:02:14 -0700

    changed the rent record a bit so it would work with my rentInputScreen class

--- src/main/RentRecord.java
+++ src/main/RentRecord.java
@@ -1,23 +1,26 @@
 package main;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class RentRecord{
     public void display(){
+        System.out.println("apt\t"+"Jan\t"+"Feb\t"+"Mar\t"+"Apr\t"+"May\t"+"Jun\t"+"Jul\t"+"Aug\t"+"Sep\t"+"Oct\t"+"Nov\t"+"Dec\t");
         try {
-            File myObj = new File("main/rentRecord");
+            File myObj = new File("C:\\Users\\Damar\\Documents\\NetBeansProjects\\CECS-343-Project\\CECS-343-Project\\src\\main\\java\\rentRecord.txt");
             Scanner scan = new Scanner(myObj);
             System.out.println(scan.nextLine());
             System.out.println(scan.nextLine());
-
+            
             while (scan.hasNextLine()) {
                 String data = scan.nextLine();
                 String[] rent = data.split(",");
-                System.out.println(rent[0] + " " + rent[1] + " " + rent[2] + " " + rent[3] +  " " +
-                        rent[4] + " " + rent[5] + " " + rent[6] + " " + rent[7] + " " + rent[8] + " " + rent[9] +
-                        " " + rent[10] + " " + rent[11] + " " + rent[12]);
+                System.out.println(data);
+                
+                
             }
             System.out.println();
             getSumOfRent();
@@ -30,30 +33,21 @@ public class RentRecord{
     }
 
     public float getSumOfRent(){
+        float total = 0;
         try {
-            File myObj = new File("main/rentRecord");
-            Scanner scan = new Scanner(myObj);
-            scan.nextLine();
-            scan.nextLine();
-
-            int totalRent = 0;
-            while (scan.hasNextLine()) {
-                String data = scan.nextLine();
-                String[] rent = data.split(",");
-
-                int yearlyRent = Integer.parseInt(rent[0]) * -1;
-                for(String s : rent){
-                    yearlyRent += Integer.parseInt(s.replaceAll(" ", ""));
+            Scanner read = new Scanner(new BufferedReader(new FileReader("C:\\Users\\Damar\\Documents\\NetBeansProjects\\CECS-343-Project\\CECS-343-Project\\src\\main\\java\\rentRecord.txt")));
+            while(read.hasNext())
+            {
+                if(read.hasNextFloat())
+                {
+                    total += read.nextFloat();
                 }
-
-                totalRent += yearlyRent;
-                // System.out.println("Yearly rent of AptNo: " + rent[0]
-                //        + ": " + yearlyRent);
+                else
+                    read.next();   
             }
-            scan.close();
-           // System.out.println("Total rent: " + totalRent);
-           // System.out.println();
-            return Float.parseFloat(String.valueOf(totalRent));
+                
+            read.close();
+            System.out.println("yearly renttotal:$ " + total+"\n");
         } catch (FileNotFoundException e) {
             System.out.println("File not found.");
             e.printStackTrace();
